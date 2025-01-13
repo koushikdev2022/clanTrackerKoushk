bac5b1c6e3ddc252ca2eb495392393948c7930a6
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-13 17:42:17 +0530

    new

--- src/helper/orderDetails.js
+++ src/helper/orderDetails.js
@@ -1,11 +1,30 @@
 
-const { Order } = require("../models");
-
+const { Order, OrderItems,UserAddress,Product,ProductImage } = require("../models");
 
 const getAllOrders = async () => {
     try {
-        const orders = await Order.findAll();
-        return orders;
+        // Directly reference the OrderItems model
+        const orderItems = await OrderItems.findAll({
+            include:[{
+                model:Order,
+                as:"Order",
+                required:false,
+                include:[{
+                    model:UserAddress,
+                    as:"UserAddress",
+                    required:false,
+                }]
+            },{
+                model:Product,
+                as:"Product",
+                include:[{
+                    model:ProductImage,
+                    as:"ProductImage",
+                    required:false,
+                }]
+            }]
+        });
+        return orderItems;
     } catch (error) {
         console.error("Error fetching orders:", error.message);
         throw error;
@@ -13,3 +32,4 @@ const getAllOrders = async () => {
 };
 
 module.exports = { getAllOrders };
+
